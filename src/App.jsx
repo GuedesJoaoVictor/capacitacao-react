1740b53d1e4db7827b7978580e836c3d0d25795a
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-01-28 15:06:35 -0300

    Adicionando taks

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
 
-function App() {
-  const [count, setCount] = useState(0)
+const dataTasks = [
+  {
+    id: 1,
+    text: "Estudar programação",
+    description:
+      "Estudar programação para se tornar um desenvolvedor FullStack.",
+    isCompleted: false,
+  },
+  {
+    id: 2,
+    text: "Estudar inglês",
+    description: "Estudar inglês para se tornar fluente.",
+    isCompleted: false,
+  },
+  {
+    id: 3,
+    text: "Estudar matemática",
+    description:
+      "Estudar matemática para se tornar um desenvolvedor FullStack.",
+    isCompleted: false,
+  },
+];
+
+export default function App() {
+  const [tasks, setTasks] = useState(dataTasks);
+
+  const onTaskClick = (taskId) => {
+    const newTasks = tasks.map((task) => {
+      // Precisa atualizar a tarefa
+      if (task.id === taskId) {
+        return {
+          ...task,
+          isCompleted: !task.isCompleted,
+        };
+      }
+
+      // Não é preciso atualizar a tarefa
+      return task;
+    });
+
+    setTasks(newTasks);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask />
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
