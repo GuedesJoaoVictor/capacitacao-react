5ffb89e2fa52788f0662a767ce55f667d14ea982
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-01-28 17:35:56 -0300

    Criando página de task description

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,16 @@
 import { ChevronRightIcon, DeleteIcon, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 function Tasks({ tasks, onTaskClick, onClickDeleteTask }) {
+  const navigate = useNavigate();
+
+  const onSeeDetailsClick = (task) => {
+    const query = new URLSearchParams();
+    query.set("title", task.text);
+    query.set("description", task.description);
+    navigate(`/task?${query.toString()}`);
+  };
+
   return (
     <div className="">
       <ul className="space-y-4 mt-4 p-6 bg-slate-200 rounded-md shadow">
@@ -14,7 +24,10 @@ function Tasks({ tasks, onTaskClick, onClickDeleteTask }) {
             >
               {task.text}
             </button>
-            <button className="bg-slate-400 p-2 rounded-md text-white">
+            <button
+              onClick={() => onSeeDetailsClick(task)}
+              className="bg-slate-400 p-2 rounded-md text-white"
+            >
               <ChevronRightIcon />
             </button>
             <button
